cb1dac159d2557f6ab7b1a0851afa83226a23416
asolanki24 <68270288+asolanki24@users.noreply.github.com>
2020-07-23 19:22:26 +1000

    updated code

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -4,7 +4,7 @@ import * as Utils from './../helper/utils.js';
 export const Calculator = () => {
     const [from, setFrom] = useState("AUD");
     const [to, setTo] = useState("AUD");
-    const [baseRate, setBaseRate] = useState(0);
+    const [baseRate, setBaseRate] = useState(1);
     const [amount, setAmount] = useState(0);
     const [errorMsg, seterrorMsg] = useState(false);
     const [srcAmount, setSrcAmount] = useState(0);
@@ -21,7 +21,7 @@ export const Calculator = () => {
         setSrcAmount(0);
 
     }
-    
+
     const onTargetCurrencyChange = (e) => {
         const toValue = e.target.value;
         let rate = 1;
@@ -48,19 +48,19 @@ export const Calculator = () => {
     }
     return (
         <div className="maindv">
-            <div className="row">
+            <div className="row rowFirst">
                 <label htmlFor="sourceCurrSeelct">Source :</label>
-                <select data-testid="sourceCurrSeelct"  onChange={(e) => onSourceCurrencyChange(e)}>
+                <select data-testid="sourceCurrSeelct" onChange={(e) => onSourceCurrencyChange(e)}>
                     {getCountryOptions(Countries.countries)}
                 </select>
-                <label htmlFor="txtAmount">Amount</label><input value={srcAmount} data-testid="txtAmount" type="number" onChange={(e) => calculateAmount(e)}></input>
+                <label htmlFor="txtAmount">Enter Amount</label><input className="txtControl" value={srcAmount} data-testid="txtAmount" type="number" onChange={(e) => calculateAmount(e)}></input>
             </div>
             <div className="row">
                 <label htmlFor="currToConvertSelect">Target  : </label>
                 <select data-testid="currToConvertSelect" onChange={(e) => onTargetCurrencyChange(e)}>
                     {getCountryOptions(Countries.countries)}
                 </select>
-                <label htmlFor="txtConvAmount">Amount</label> <input data-testid="txtConvAmount" readOnly type="number" value={amount}></input>
+                <label htmlFor="txtConvAmount">Converted Amount</label> <input className="txtControl" data-testid="txtConvAmount" readOnly type="number" value={amount}></input>
             </div>
             <div id="dvError" className={errorMsg ? 'showErrDV' : 'hideErrDV'}>
                 <span>Conversion not available!!</span>
